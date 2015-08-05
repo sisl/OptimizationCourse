b1ec5e066766c3258e55c0a9ff76621f4e2bca54
Renee Trochet <rtrochet@gmail.com>
2015-08-05 10:33:11 -0700

    Lesson 5-01

---
+++ Unit5-ConstrainedWithCalc/files/answers.jl
@@ -0,0 +1,62 @@
+using Revealables
+
+#############
+# Lesson 01 #
+#############
+
+ans501A = Revealable("""
+###Answer A
+1. The feasible region has 5 corner points. On the left side it is bounded mainly by the parabola, though there is a crossing point with the 1/x near the top of the region. The upper left corner is the intersection of 1/x with 10-x; the entire upper bound is the line, and the right boundary is the parabola. The lower boundary is 1/x which crosses the parabola twice, close to x = 3. 
+2. The maximum value will likely occur at the right corner (between the parabola and line).<br /><br />
+The minimum value will probably occur at the corner closest to the origin, the middle of three crossing points of the parabola with 1/x.<br /><br />
+Hopefully two lessons will be learned here: first, this is harder than it seems at first glance, and second, we're really just guessing.
+""", "Answer", false)
+
+ans501B = Revealable("""
+###Answer B
+Minimization occurs at g (0, 2) with a minimum of -6
+
+Maximization occurs at c (4.726, 1.181) with a maximum of 41.12
+""", "Answer", false)
+
+ans501C = Revealable("""
+###Answer C
+1. -x<sub>2</sub> + e<sup>-x<sub>1</sub></sup> + 2 &le; 0
+2. x<sub>1</sub><sup>2</sup> + x<sub>2</sub><sup>2</sup> - 24 &le; 0 
+3. x<sub>1</sub><sup>2</sup> - 4x<sub>1</sub> + x<sub>2</sub> - 2 &le; 0
+4. -x<sub>1</sub> &le; 0
+5. -x<sub>2</sub> &le; 0
+
+If you graph these all together, it's a four-sided region (the axes do not come into play) bounded left and right by the parabola, top by the circle and bottom by the exponential.
+""", "Answer", false)
+
+ans501D = Revealable("""
+###Answer D
+f(x) = 3x<sub>1</sub><sup>2</sup> - 2x<sub>1</sub>x<sub>2</sub>
+
+g<sub>1</sub>(x) = x<sub>1</sub><sup>2</sup> + 5 - x<sub>2</sub> &le; 0
+
+g<sub>2</sub>(x) = 12 - x<sub>1</sub><sup>2</sup> - x<sub>2</sub><sup>2</sup> &le; 0
+
+g<sub>3</sub>(x) = -x<sub>1</sub> &le; 0
+
+g<sub>4</sub>(x) = -x<sub>2</sub> &le; 0
+
+h(x) = x<sub>2</sub><sup>2</sup> + 4x<sub>1</sub><sup>2</sup> - 18 = 0
+""", "Answer", false)
+
+ans501E = Revealable("""
+###Answer E
+(-1.977, -0.303) and (1.95369, 0.428)
+
+(0.159, 0.608), (3.145, 2.689), and (-0.552, -2.513)
+""", "Answer", false)
+
+#############
+# Lesson 02 #
+#############
+
+ans502A = Revealable("""
+###Answer A
+
+""", "Answer", false)
