afe4f3bf066c02af8bc0e40b92164a6fd265a1b8
Renee Trochet <rtrochet@gmail.com>
2015-08-07 12:08:29 -0700

    ch 1: answers and extras split

--- Unit1-Intro/files/answers.jl
+++ Unit1-Intro/files/answers.jl
@@ -97,21 +97,6 @@ end
 # Lesson 08 #
 #############
 
-defIteration = Revealable("""
-__Iteration__ is when the same procedure is repeated multiple times.
-
-
-Some examples were long division, the Fibonacci numbers, prime numbers, and the calculator game. Some of these used recursion as well, but not all of them.
-""", "Definition", false)
-
-ext1081 = Revealable("""
-You would change it to `function Sum(a,b)`, and modify the \"for\" line to `for x in a:b`.
-""", "Extension Answer", false)
-
-ext1082 = Revealable("""
-Change it to `println(\"The sum is \$S\"`
-""", "Extension Answer", false)
-
 ans108A = Revealable("""
 ###Answer A
 Sample code:
